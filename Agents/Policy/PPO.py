Fix loop in backprop graph

Agents/Policy/PPO.py
@@ -44,7 +44,7 @@ class AdaptativePPO(A2C):
         dkl = batched_dkl(new_pi, pi)
 
         # computing the adjusted loss
-        return advantage_loss - self.beta * dkl
+        return -(advantage_loss - self.beta * dkl)
 
     def learn(self, done):
         batches = self.memory.sample_batch(batch_size=self.batch_size)
@@ -55,9 +55,11 @@ class AdaptativePPO(A2C):
         b_new = batches['new_obs']
         b_done = batches['done']
 
-        pi = self.model.policy(b_obs)
-        values = self.model.critic(b_obs).squeeze()
         with torch.no_grad():
+            # compute policy and critic
+            pi = self.model.policy(b_obs)
+            values = self.model.critic(b_obs).squeeze()
+
             # compute td0
             next_critic = self.model.critic(b_new)
             td0 = (b_reward + self.discount * next_critic.squeeze() * (~b_done)).float()

TP/TP6/main_PPO.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     episode_count = config["nbEpisodes"]
 
     agent = {
-             'PPO'          : AdaptativePPO(env, config, layers=[30, 30], k=1, memory_size=100, batch_size=100),
+             'PPO'          : AdaptativePPO(env, config, layers=[256], k=10, memory_size=100, batch_size=100),
              'ClippedPPO'   : ClippedPPO(env, config, layers=[30, 30], k=10, memory_size=100, batch_size=100)
              }[mode]
 
@@ -88,7 +88,7 @@ if __name__ == '__main__':
                 'obs': ob,
                 'action': action,
                 'new_obs': torch.from_numpy(new_ob),
-                'reward': reward/100,   # rescale factor for NN
+                'reward': reward,   # rescale factor for NN
                 'done': done,
                 'it': j
             }