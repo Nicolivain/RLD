0506a2621712236599f15d0748b0e8b2496f92e9
Paul Liautaud <paul.liautaud04@gmail.com>
2021-12-04 14:11:36 +0100

    adding UCB-V

--- TP/TP1/main_bandits.py
+++ TP/TP1/main_bandits.py
@@ -46,8 +46,25 @@ if __name__ == '__main__':
 
     print(f"UCB:    {g/5000}")
 
+    # UCB-V
+
+    print('\n--- Average click rate for UCB-V based policies ---\n')
+    g = 0
+    m = UCB(10, var=True)
+
+    for i in range(data.shape[0]):
+        rewards = np.array([float(e) for e in data[i].split(":")[2].split(';')])
+        choosen = m.pick_arm(i + 1)
+        m.arms[choosen].update(rewards[choosen])
+        g += rewards[choosen]
+
+    print(f"UCB-V:    {g / 5000}")
+
+
+
     # LinUCB
 
+    print('\n--- Average click rate for LinUCB based policies ---\n')
     g = 0
     m = LinUCB(10, 5, 0.5)
 
