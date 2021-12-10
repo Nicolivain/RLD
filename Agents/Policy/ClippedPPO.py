b6c2103c3ae95556236585a0b979d930b355d085
Nicolas Olivain <nicolasolivain@live.com>
2021-12-10 21:30:29 +0100

    misc

--- Agents/Policy/ClippedPPO.py
+++ Agents/Policy/ClippedPPO.py
@@ -5,7 +5,7 @@ from Agents.Policy.PPO import AdaptativePPO
 
 class ClippedPPO(AdaptativePPO):
     def __init__(self, env, opt, layers, k, epsilon=0.001, delta=1.5, loss='smoothL1', memory_size=1000, batch_size=1000, use_dkl=True, reversed_dkl=False, **kwargs):
-        super(ClippedPPO, self).__init__(env, opt, layers, k ,delta, loss, memory_size, batch_size)
+        super(ClippedPPO, self).__init__(env, opt, layers, k, delta, loss, memory_size, batch_size)
         self.epsilon = epsilon
 
     def _compute_objective(self, advantage, pi, new_pi, new_action_pi, action_pi):
