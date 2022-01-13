67f4376e318faafc2d362d13a2adb572b02aa9ea
Nicolas Olivain <nicolasolivain@live.com>
2022-01-13 11:25:48 +0100

    working VAE

--- TP/TP10/VAE.py
+++ TP/TP10/VAE.py
@@ -2,25 +2,29 @@ import torch
 
 
 class VAE(torch.nn.Module):
-    def __init__(self, in_feature, lattent_space, hidden_layers=[]):
+    def __init__(self, in_feature, lattent_space, hidden_layers=[], device='cpu'):
         super().__init__()
 
         self.in_features = in_feature
         self.lattent_space = lattent_space
 
-        enc = [in_feature] + hidden_layers + lattent_space
+        self.device = device
+
+        enc = [in_feature] + hidden_layers + [lattent_space]
         self.encoder_layers = torch.nn.ModuleList([torch.nn.Linear(i, o) for (i, o) in zip(enc[:-1], enc[1:])])
 
         self.mu  = torch.nn.Linear(lattent_space, lattent_space)
         self.logsigma = torch.nn.Linear(lattent_space, lattent_space)
 
-        enc = enc.reverse()
+        enc.reverse()
         self.decoder_layers = torch.nn.ModuleList([torch.nn.Linear(i, o) for (i, o) in zip(enc[:-1], enc[1:])])
 
         self.relu = torch.nn.ReLU()
         self.sigmoid = torch.nn.Sigmoid()
 
-    def forward(self, x):
+        self.to(self.device)
+
+    def encode(self, x):
         for layer in self.encoder_layers:
             x = layer(x)
             x = self.relu(x)
@@ -28,10 +32,31 @@ class VAE(torch.nn.Module):
 
         mu  = self.mu(x)
         logsigma = self.logsigma(x)
-        z = mu + logsigma.exp() * torch.randn(self.lattent_space)
+        z = mu + logsigma.exp() * torch.randn(self.lattent_space, device=self.device)
 
+        return z, mu, logsigma.exp()
+
+    def decode(self, z):
         for layer in self.decoder_layers:
             z = layer(z)
             z = self.relu(z)
         z = self.sigmoid(z)
         return z
+
+    def forward(self, x):
+        z, _, _ = self.encode(x)
+        xhat = self.decode(z)
+        return xhat
+
+    @staticmethod
+    def kl_divergence(z, mu, std):
+        # KL divergence for lattent space regularization
+        p = torch.distributions.Normal(torch.zeros_like(mu), torch.ones_like(std))
+        q = torch.distributions.Normal(mu, std)
+
+        log_qzx = q.log_prob(z)
+        log_pz = p.log_prob(z)
+
+        kl = (log_qzx - log_pz)
+        kl = kl.sum(-1)
+        return kl.mean()
--- TP/TP10/main.py
+++ TP/TP10/main.py
@@ -1,14 +1,53 @@
 import torch
 import torchvision.datasets as dset
+import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from VAE import VAE
 
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+n_epoch = 10
 batch_size = 1000
+lr = 0.001
 
-dataset = dset.MNIST(root='data', download=True)
+dataset = dset.MNIST(root='data', download=True, transform=transforms.ToTensor())
 dataloader = DataLoader(dataset, batch_size=batch_size)
+
+model = VAE(784, 100, hidden_layers=[512, 254, 128], device=device)
+
+criterion = torch.nn.BCELoss()
+opt = torch.optim.Adam(params=model.parameters(), lr=lr)
+
+for epoch in range(n_epoch):
+    epoch_kll  = 0
+    epoch_bce  = 0
+    epoch_loss = 0
+    for batch in dataloader:
+        batch_x = batch[0].to(device)
+        batch_x = batch_x.view(batch_x.shape[0], -1)
+
+        z, mu, std = model.encode(batch_x)
+        KLloss = model.kl_divergence(z, mu, std)
+
+        xhat = model.decode(z)
+
+        bce = criterion(xhat, batch_x)
+
+        loss = bce + KLloss
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+
+        epoch_kll += KLloss.item()
+        epoch_bce += bce.item()
+        epoch_loss += loss.item()
+
+    epoch_loss /= len(dataloader)
+    epoch_bce  /= len(dataloader)
+    epoch_kll  /= len(dataloader)
+
+    print(f'Epoch {epoch}: Loss {epoch_loss}, BCE {epoch_bce}, KLL {epoch_kll}')
+
 
 
 
