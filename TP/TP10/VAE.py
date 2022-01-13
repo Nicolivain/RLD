98de950c3c20ef0b54ff035eccb96bebca5c98c9
Nicolas Olivain <nicolasolivain@live.com>
2022-01-13 19:58:06 +0100

    added convVAE

--- TP/TP10/VAE.py
+++ TP/TP10/VAE.py
@@ -56,3 +56,74 @@ class VAE(PytorchVAE):
         # KL divergence for lattent space regularization
         kld = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
         return kld
+
+
+class ConvVAE(PytorchVAE):
+    def __init__(self, image_shape, in_channels, lattent_space, hidden_filters=[], kernel_size=(2, 2), strides=(1, 1), device='cpu', logger=None):
+        super().__init__(device=device, logger=logger)
+
+        self.image_shape = image_shape
+        self.n_pixel = image_shape[0] * image_shape[1]
+        self.in_channels = in_channels
+        self.lattent_space = lattent_space
+
+        self.ks = kernel_size
+        self.strides = strides
+
+        enc = [in_channels] + hidden_filters + [1]
+        self.encoder_layers = torch.nn.ModuleList([torch.nn.Conv2d(i, o, stride=self.strides, kernel_size=self.ks) for (i, o) in zip(enc[:-1], enc[1:])])
+
+        h_out, w_out = self.get_output_shape(image_shape[0], image_shape[1], len(enc)-1)
+        self.h_out = h_out
+        self.w_out = w_out
+
+        self.mu           = torch.nn.Linear(h_out * w_out, self.lattent_space)
+        self.logvar       = torch.nn.Linear(h_out * w_out, self.lattent_space)
+        self.decode_ready = torch.nn.Linear(self.lattent_space, h_out * w_out)
+
+        enc.reverse()
+        self.decoder_layers = torch.nn.ModuleList([torch.nn.ConvTranspose2d(i, o, stride=self.strides, kernel_size=self.ks) for (i, o) in zip(enc[:-1], enc[1:])])
+
+        self.relu = torch.nn.ReLU()
+        self.sigmoid = torch.nn.Sigmoid()
+
+        self.to(self.device)
+
+    def encode(self, x):
+        for layer in self.encoder_layers:
+            x = layer(x)
+            x = self.relu(x)
+        x = x.view(x.shape[0], -1)
+
+        mu  = self.mu(x)
+        logvar = self.logvar(x)
+
+        std = torch.exp(0.5 * logvar)  # standard deviation
+        eps = torch.randn_like(std)  # `randn_like` as we need the same size
+        z = mu + (eps * std)  # sampling as if coming from the input spac
+
+        return z, mu, logvar
+
+    def decode(self, z):
+        z = self.decode_ready(z).view(z.shape[0], 1, self.h_out, self.w_out)
+        for layer in self.decoder_layers:
+            z = layer(z)
+        z = self.sigmoid(z)
+        return z
+
+    def forward(self, x):
+        z, _, _ = self.encode(x)
+        xhat = self.decode(z)
+        return xhat
+
+    @staticmethod
+    def lattent_reg(mu, logvar):
+        # KL divergence for lattent space regularization
+        kld = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+        return kld
+
+    def get_output_shape(self, hin, win, n_conv):
+        for k in range(n_conv):
+            hin = (hin - (self.ks[0] - 1) - 1)/self.strides[0] + 1
+            win = (win - (self.ks[1] - 1) - 1)/self.strides[1] + 1
+        return int(hin), int(win)
--- TP/TP10/main.py
+++ TP/TP10/main.py
@@ -2,7 +2,7 @@ import torch
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
-from VAE import VAE
+from VAE import VAE, ConvVAE
 
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -12,10 +12,21 @@ lattent = 16
 layers = [512]
 lr = 0.0001
 
+
 train_dataset = dset.MNIST(root='data', download=True, transform=transforms.Compose([transforms.ToTensor(), transforms.Lambda(lambda x: torch.flatten(x))]), train=True)
 dataloader = DataLoader(train_dataset, batch_size=batch_size)
 v_dataset = dset.MNIST(root='data', download=True, transform=transforms.Compose([transforms.ToTensor(), transforms.Lambda(lambda x: torch.flatten(x))]), train=False)
 v_dataloader = DataLoader(v_dataset, batch_size=batch_size)
 
 model = VAE(784, lattent, hidden_layers=layers, device=device, logger=None)
+model.fit(dataloader, validation_data=v_dataloader, lr=lr, n_epochs=n_epoch, save_images_freq=1)
+
+layers = [4]  # we use less filters
+
+train_dataset = dset.MNIST(root='data', download=True, transform=transforms.Compose([transforms.ToTensor()]), train=True)
+dataloader = DataLoader(train_dataset, batch_size=batch_size)
+v_dataset = dset.MNIST(root='data', download=True, transform=transforms.Compose([transforms.ToTensor()]), train=False)
+v_dataloader = DataLoader(v_dataset, batch_size=batch_size)
+
+model = ConvVAE(image_shape=(28, 28), in_channels=1, lattent_space=lattent, hidden_filters=layers, device=device, logger=None)
 model.fit(dataloader, validation_data=v_dataloader, lr=lr, n_epochs=n_epoch, save_images_freq=1)
