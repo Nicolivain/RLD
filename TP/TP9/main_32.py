8193de22db2f75e0f08a278a71512d2b4f291100
Nicolas Olivain <nicolasolivain@live.com>
2022-01-20 19:26:17 +0100

    small adjustments

--- TP/TP9/main_32.py
+++ TP/TP9/main_32.py
@@ -1,4 +1,5 @@
 import torch
+import os
 import logging
 import datetime
 import numpy as np
@@ -14,7 +15,8 @@ from DCGAN_32 import DCGAN_32
 
 logging.basicConfig(level=logging.INFO)
 start_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-writer = SummaryWriter("TP/TP9/runs/tag-" + start_time + '_MNIST')
+save_path = "TP/TP9/runs/tag-" + start_time + '_MNIST'
+writer = SummaryWriter(save_path)
 
 image_size = 32
 
@@ -23,7 +25,9 @@ ndf = 64
 nz  = 100
 ngf = 64
 
-dataset = dset.MNIST(root="../../../data",
+path = os.path.abspath("../../../data")
+dataset = dset.MNIST(root=path,
+                     download=True,
                      transform=transforms.Compose([
                          transforms.Resize(image_size),
                          transforms.CenterCrop(image_size),
@@ -34,6 +38,7 @@ print(dataset)
 seed = 0
 torch.manual_seed(seed)
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print('Device: ', device)
 
 batch_size = 128
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=1)
@@ -42,11 +47,11 @@ model = DCGAN_32(n_channels=nc, lattent_space_size=nz, n_disc_filters=ndf, n_gen
 
 model.fit(dataloader, n_epochs=5, lr=1e-3)
 
-noise = torch.randn(64, nz, 1, 1, device=device)
+noise = torch.randn(32, nz, 1, 1, device=device)
 fake = model.generate(noise)
 img = vutils.make_grid(fake, padding=2, normalize=True)
 plt.figure(figsize=(15, 15))
 plt.axis("off")
 plt.title("Fake Images")
 plt.imshow(np.transpose(img.cpu(), (1, 2, 0)))
-plt.savefig("fake.png")
+plt.savefig(os.path.join(save_path, "fake.png"))
