432c768c89316c65bf1d6ae81ca8ed06e3132cd7
Paul Liautaud <paul.liautaud04@gmail.com>
2022-02-26 16:19:52 +0100

    update NothingToDo - adapted to all envs

--- TP/TP11/main_MADDPG.py
+++ TP/TP11/main_MADDPG.py
@@ -9,15 +9,13 @@ from Core.Trainer import *
 
 if __name__ == '__main__':
 
-    #CONSEIL POUR DEBUG : copier le fichier simple_spread.py pour mettre le nombre de cible à 1 et avec 1 un seul agent
-
     # Choose your map env in the config file
 
     env, _, world, config, outdir, logger = init_MADDPG('Config/env_config/config_multiagent.yaml', 'MADDPG', outdir=None,
                                        copy_config=False, launch_tb=False)
 
     params = load_model_params('MADDPG', env, config, world)
-    print(params)
+
     agent = MADDPG
     xp = Trainer_MADDPG(agent=agent,
                  env=env,
